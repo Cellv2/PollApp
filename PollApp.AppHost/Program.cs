2983ade54a45156139c6ea85240732cb0c1feb6b
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2025-02-03 21:08:34 +0000

    Add API project and rename program.cs files

--- PollApp.AppHost/Program.cs
+++ PollApp.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.PollApp_UI>("pollapp-ui");
 
+builder.AddProject<Projects.PollApp_Server_API>("pollapp-server-api");
+
 builder.Build().Run();
