fd2cfdf99d8b666566c842a2fd293cf0b2f9dc30
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2025-02-04 22:34:21 +0000

    Recreate as .net8 due to bUnit not supporting .net9 right now

--- PollApp.UI/UI.cs
+++ PollApp.UI/UI.cs
@@ -22,10 +22,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
